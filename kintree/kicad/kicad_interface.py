8f4392b343f62e9b45ab4dff766e032992487d24
eeintech <eeintech@eeinte.ch>
2022-01-06 08:21:32 -0500

    Starting migration to KiCad v6

--- kintree/kicad/kicad_interface.py
+++ kintree/kicad/kicad_interface.py
@@ -1,17 +1,17 @@
-from . import kicad_schlib
-
-klib = kicad_schlib.ComponentLibManager()
-
+from . import kicad_symbol
 
 def inventree_to_kicad(part_data: dict, library_path: str, template_path=None, show_progress=True) -> bool:
     ''' Create KiCad symbol from InvenTree part data '''
-    return klib.add_component_to_library_from_inventree(component_data=part_data,
-                                                        library_path=library_path,
-                                                        template_path=template_path,
-                                                        show_progress=show_progress)
+    klib = kicad_symbol.ComponentLibManager(library_path)
+    if klib:
+        return klib.add_component_to_library_from_inventree(component_data=part_data,
+                                                            template_path=template_path,
+                                                            show_progress=show_progress)
+    else:
+        return False, False
 
-
-def delete_part(part_number: str, library_path: str) -> bool:
-    ''' Delete KiCad symbol from library '''
-    return klib.delete_component_from_lib(part_number=part_number,
-                                          library_path=library_path)
+# NOT SUPPORTED YET
+# def delete_part(part_number: str, library_path: str) -> bool:
+#     ''' Delete KiCad symbol from library '''
+#     return klib.delete_component_from_lib(part_number=part_number,
+#                                           library_path=library_path)
