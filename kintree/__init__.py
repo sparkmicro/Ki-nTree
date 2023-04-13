671e5d1e773016086dea8e768453eaf8f9295ccb
eeintech <eeintech@eeinte.ch>
2023-04-13 14:30:13 -0400

    another test 2

--- kintree/__init__.py
+++ kintree/__init__.py
@@ -5,7 +5,7 @@
 version_info = {
     'MAJOR_REVISION': 1,
     'MINOR_REVISION': 0,
-    'RELEASE_STATUS': '0fork',
+    'RELEASE_STATUS': '0test',
 }
 
 __version__ = '.'.join([str(v) for v in version_info.values()])
